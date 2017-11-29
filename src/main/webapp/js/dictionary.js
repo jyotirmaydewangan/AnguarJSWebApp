0afd54b16db7259b834a022e34e52d351a267e73
Jyotirmay Dewangan <jyotirmay.d@VS-C02QN4AKG8WN.local>
2017-11-29 18:15:50 +0530

    refactor

--- src/main/webapp/js/dictionary.js
+++ src/main/webapp/js/dictionary.js
@@ -3,6 +3,7 @@ var dictionaryApp = angular.module('dictionaryApp', ['ngResource', 'ui.bootstrap
     $dialogProvider.options({backdropClick: false, dialogFade: true});
 });
 
+
 dictionaryApp.factory('DictionaryResource', function ($resource) {
     return $resource('/api/word/:word/:lang', {}, {});
 });
@@ -17,7 +18,134 @@ dictionaryApp.filter('capitalize', function() {
     }
 });
 
+
+dictionaryApp.filter('description', function() {
+    return function(input) {
+
+        switch(input) {
+            case "hyponym" :
+                text = "A word that is more specific than a given word.";
+                break;
+            case "hypernym" :
+                text = "A word that is more generic than a given word.";
+                break;
+            case "part holonym" :
+                text = "A word that names the whole of which a given word is a part.";
+                break;
+            case "domain member category" :
+                text = input;
+                break;
+            case "substance holonym" :
+                text = input;
+                break;
+            case "part meronym" :
+                text = "A word that names a part of a larger whole.";
+                break;
+            case "domain category" :
+                text = input;
+                break;
+            case "member meronym" :
+                text = input;
+                break;
+            case "instance hyponym" :
+                text = input;
+                break;
+            case "attribute" :
+                text = input;
+                break;
+            case "domain usage" :
+                text = input;
+                break;
+            case "instance hypernym" :
+                text = input;
+                break;
+            case "domain region" :
+                text = input;
+                break;
+            case "member holonym" :
+                text = input;
+                break;
+            case "domain member usage" :
+                text = input;
+                break;
+            case "substance meronym" :
+                text = input;
+                break;
+            case "domain member region" :
+                text = input;
+                break;
+            case "verb group" :
+                text = input;
+                break;
+            case "entail" :
+                text = input;
+                break;
+            case "cause" :
+                text = input;
+                break;
+            case "also" :
+                text = input;
+                break;
+            case "similar" :
+                text = input;
+                break;
+            case "pertainym" :
+                text = input;
+                break;
+            case "antonym" :
+                text = input;
+                break;
+            case "derivation" :
+                text = input;
+                break;
+            case "also" :
+                text = input;
+                break;
+            case "domain usage" :
+                text = input;
+                break;
+            case "domain region" :
+                text = input;
+                break;
+            case "domain member usage" :
+                text = input;
+                break;
+            case "participle" :
+                text = input;
+                break;
+            case "verb group" :
+                text = input;
+                break;
+            case "domain member category" :
+                text = input;
+                break;
+            case "domain member region" :
+                text = input;
+                break;
+            case "domain category" :
+                text = input;
+                break;
+            default:
+                text = "default";
+        }
+
+
+        return text;
+    }
+});
+
 function DictionaryCtrl($scope, $routeParams, DictionaryResource, ReverseDictionaryResource) {
+
+    $scope.roles = ["derivation", "hypernym", "hyponym", "holonym",
+                    "meronym", "also", "similar", "antonym"];
+
+    $scope.relatedByFilter = function(role){
+        var indexOfRole = $scope.roles.indexOf(role); // or whatever your object is instead of $scope.roles
+        if (indexOfRole === -1)
+            return false;
+        else
+            return true;
+    }
 
     $scope.DictionaryForm = {
         show: true,
@@ -25,8 +153,8 @@ function DictionaryCtrl($scope, $routeParams, DictionaryResource, ReverseDiction
     }
 
     $scope.DictionaryForm.text.word = $routeParams.word;
-
     $scope.searchBoxWord = '';
+    $scope.targetLang = '';
 
     $scope.noun = {
         show: false
@@ -116,7 +244,6 @@ function DictionaryCtrl($scope, $routeParams, DictionaryResource, ReverseDiction
             $scope.adjective.show = false;
             $scope.other.show = false;
             $scope.example.show = false;
-
             $scope.preposition.show = false;
             $scope.conjunction.show = false;
             $scope.pronoun.show = false;
